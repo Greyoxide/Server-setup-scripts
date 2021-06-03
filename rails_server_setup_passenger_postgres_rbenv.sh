fc26ea92e8e40601f5fb6750fafe0456f9f9c77e
greyoxide <greyoxide@gmail.com>
2021-06-03 13:23:57 -0700

    broke user script out into seperate script

--- rails_server_setup_passenger_postgres_rbenv.sh
+++ rails_server_setup_passenger_postgres_rbenv.sh
@@ -51,26 +51,4 @@ cd /home/deploy
 #add user to postgresql
 sudo -u postgres createuser deploy
 
-su deploy
-
-echo "User: $PASS" >> out.txt
-
-#generate ssh key to pull
-ssh-keygen -t rsa -N "" -f '~/.ssh/id_rsa'
-
-echo 'SSH key generated'
-
-#setup Rbenv
-
-git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-
-git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
-echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
-
-echo 'RBENV installed'
-
-cd && git clone https://github.com/kolbasa/git-repo-watcher
-
 exit 0
